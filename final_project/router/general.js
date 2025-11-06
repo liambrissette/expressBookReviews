c7b44a5306eb225cef4c38e166c68cac4e710dce
liambrissette <liambrissette@gmail.com>
2025-11-05 21:30:13 -0500

    Task 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,10 +31,10 @@ public_users.get('/',function (req, res) {
         }
     })
     .then(bookList => {
-        return res.status(200).json({ message: bookList });
+        return res.status(300).json({ message: bookList });
     })
     .catch(err => {
-        return res.status(500).json({ message: err });
+        return res.status(300).json({ message: err });
     });
 });
 
@@ -54,38 +54,57 @@ public_users.get('/isbn/:isbn',function (req, res) {
 
     getBookByISBN
         .then(book => {
-            res.status(200).json({ message: book });
+            res.status(300).json({ message: book });
         })
         .catch(err => {
-            res.status(404).json({ message: err });
+            res.status(300).json({ message: err });
         });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const reqAuthor = req.params.author;
-    let booksToReturn = [];
 
-   Object.values(books).forEach(book => {
-        if (reqAuthor == book.author) {
-            booksToReturn.push(book);
+    new Promise((resolve, reject) => {
+        let booksToReturn = [];
+
+        Object.values(books).forEach(book => {
+            if (reqAuthor === book.author) {
+                booksToReturn.push(book);
+            }
+        });
+
+        if (booksToReturn.length > 0) {
+            resolve(booksToReturn);
+        } else {
+            reject("No books found for this author");
         }
-
-   })
-   return res.status(300).json({message:  booksToReturn});
+    })
+    .then(result => res.status(300).json({ message: result }))
+    .catch(err => res.status(300).json({ message: err }));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const reqTitle = req.params.title;
-    let booksToReturn = [];
 
-    Object.values(books).forEach(book => {
-        if (reqTitle == book.title) {
-            booksToReturn.push(book);
+    new Promise((resolve, reject) => {
+        let booksToReturn = [];
+
+        Object.values(books).forEach(book => {
+            if (reqTitle === book.title) {
+                booksToReturn.push(book);
+            }
+        });
+
+        if (booksToReturn.length > 0) {
+            resolve(booksToReturn);
+        } else {
+            reject("No books found with this title");
         }
     })
-  return res.status(300).json({message:  booksToReturn});
+    .then(result => res.status(300).json({ message: result }))
+    .catch(err => res.status(300).json({ message: err }));
 });
 
 //  Get book review
